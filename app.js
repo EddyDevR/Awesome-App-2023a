d23e45638f72ea920250b518bd08e989bd4b7aff
eddy ruiz <eddyvaldo@live.com.mx>
2023-05-11 08:50:16 -0600

    add middleware

--- app.js
+++ app.js
@@ -11,6 +11,28 @@ const app = express();
 // crear el servidor 
 const server = http.createServer(app);
 
+//creando middleware esa una funcion 
+app.use((req, res, next) => {
+    console.log('😊 Todo bien');
+    // invocando al siguiente middleware
+    next();
+});
+
+
+app.use((req, res, next) => {
+    console.log('✌ Registrando peticion');
+    console.log(` ${req.method}  = ${req.url}`);
+    next();
+})
+
+app.use((req, res) => {
+    console.log('😒 Respondiendo al cliente');
+    res.send(`
+    <h1>Hello World </h1>
+    <p>This is awesome app<p/>
+    `);
+})
+
 // Definir puertos 
 const port = 3000;
 const ip = '0.0.0.0';
