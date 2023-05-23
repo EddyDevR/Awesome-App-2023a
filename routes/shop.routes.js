2aa238521ebaba8817b3f8e52c57e641ed5d6612
eddy ruiz <eruiz2959@gmail.com>
2023-05-22 22:45:51 -0600

    add views

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,6 +1,9 @@
 // Importando el enrutador de express
 import { Router } from 'express';
 
+// Importando el gestor de rutas
+import path from 'path';
+
 // Creando una instancia del enrutador de express
 const router = Router();
 
@@ -8,10 +11,7 @@ const router = Router();
 router.get('/', (req, res) => {
     console.log("📢 Sirviendo la ruta '/'");
     // Se contesta al server
-    res.send(`
-    <h1>Welcome to Express Js ♥</h1>
-    <p>This is my awesome app! 😎</p>
-    `);
+    res.sendFile(path.resolve('views', 'shop.html'));
 });
 
 // GET /about
