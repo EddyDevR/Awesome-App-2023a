99da7228b7e28620e9d221b440d40f9445dc3b22
eddy ruiz <eddyvaldo@live.com.mx>
2023-05-23 07:56:10 -0600

    add 404 view

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Importar express
 import express from 'express';
 
+// Importando http-status
+import httpStatus from 'http-status';
+
 // Importando el enrutador 
 import adminRouter from './routes/admin.routes.js';
 import shopRouter from './routes/shop.routes.js';
-
+import path from 'path';
 // Crear una instancia de express
 const app = express(); // (req,res) => {UN MONTON DE CÓDIGO}
 
@@ -15,6 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(adminRouter);
 // Se agrega la ruta shop 
 app.use(shopRouter);
+
+// registrando middleware para el error 404
+app.use((req, res) => [
+res.status(httpStatus.NOT_FOUND).sendFile(path.resolve('views','404.html'))
+])
 
 // Definir puertos
 const port = 3000;
