170b4def77562a9366fbf1e76380604ec2d1000c
eddy ruiz <eddyvaldo@live.com.mx>
2023-05-26 09:47:41 -0600

    estaticos

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ import httpStatus from 'http-status';
 // Importando el enrutador 
 import adminRouter from './routes/admin.routes.js';
 import shopRouter from './routes/shop.routes.js';
+
+// importando root dir , primer middleware en ser registrado deber ser el servidor de estaticos
+import { ROOT_DIR } from './helpers/paths.js';
+
+// importando path
 import path from 'path';
+
 // Crear una instancia de express
 const app = express(); // (req,res) => {UN MONTON DE CÓDIGO}
 
 // Middleware del parseo de datos del cliente
 app.use(express.urlencoded({ extended: true }));
+
+// se registra el middleware para servidor archivos estaticos
+app.use(express.static(path.join(ROOT_DIR, 'public')));
+
+
+
 
 // Se agrega ruta de administrador
 app.use('/admin', adminRouter);
