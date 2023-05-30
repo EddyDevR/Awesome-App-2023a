a10d2f41239806412a03c6417ae0c22e97d98c7f
eddy ruiz <eddyvaldo@live.com.mx>
2023-05-30 08:22:18 -0600

    a

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -7,21 +7,12 @@ import path from 'path';
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// Importando productos
-import { products } from './admin.routes.js';
-
 // GET /
 router.get('/', (req, res) => {
-    // Mostrando productos en memoria
-    console.log(products);
     console.log("📢 Sirviendo la ruta '/'");
-<<<<<<< HEAD
     // Se contesta al server
     res.render('shop');
     // res.render('shop', { layout: false });
-=======
-    res.sendFile(path.resolve('views', 'shop.html'));
->>>>>>> bcd0e74097f0449ba7c1802262e829ab400dc5b0
 });
 
 // GET /about
